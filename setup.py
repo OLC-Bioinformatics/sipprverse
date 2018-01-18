18565634ad74b131c1708fc064c3f546fac324be
adamkoziol <adam.koziol@inspection.gc.ca>
2018-01-18 14:15:08 -0500

    Fixed install_requires to not specify a version of certain packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.0.29",
+    version="0.0.30",
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
@@ -13,8 +13,8 @@ setup(
     description='Object oriented raw read typing software',
     url='https://github.com/OLC-Bioinformatics/geneSipprV2/sipprverse',
     long_description=open('README.md').read(),
-    install_requires=['OLCTools==0.3.19',
-                      'sipprverse==0.0.24',
+    install_requires=['OLCTools',
+                      'sipprverse',
                       'biopython==1.70',
                       'pysam==0.13'
                       ]
