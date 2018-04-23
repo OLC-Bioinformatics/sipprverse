eb77a3ed8627a7b712f5c2b4af179f9b3a6c99b7
adamkoziol <adam.koziol@inspection.gc.ca>
2018-04-23 10:11:09 -0400

    Updated environment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.0.58",
+    version="0.0.59",
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
