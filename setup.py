f4219de55bf32684bd4708da1698386e136f8a7a
Adam Koziol <adam.koziol@inspection.gc.ca>
2018-09-26 10:04:50 -0400

    Packaging update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.0.76",
+    version="0.0.77",
     packages=find_packages(),
     include_package_data=True,
     license='MIT',
