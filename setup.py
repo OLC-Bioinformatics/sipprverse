eddf4d70c907f2fc6baa0cc3e1d2de234ecdfff2
adamkoziol <adam.koziol@inspection.gc.ca>
2018-10-19 16:27:52 -0400

    Updates to database setup in olctools

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.0.88",
+    version="0.0.89",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
