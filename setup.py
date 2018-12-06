f654a90c17c9203e5c0d5eb0787fde4022e5f8c4
adamkoziol <adam.koziol@inspection.gc.ca>
2018-12-06 13:23:00 -0500

    Updating package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.14",
+    version="0.2.15",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
