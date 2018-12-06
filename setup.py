b868aa84731ace36327d0669b5a40c96c2b23e22
adamkoziol <adam.koziol@inspection.gc.ca>
2018-12-06 10:06:15 -0500

    Updating pip package and dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.13",
+    version="0.2.14",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
