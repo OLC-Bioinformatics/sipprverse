e947d90f8b8a2c8158d201846cc64370629fff0a
adamkoziol <adam.koziol@inspection.gc.ca>
2019-05-23 16:12:22 -0400

    Restructured package to use genemethods

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
 __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.27",
+    version="0.2.28",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
-	'sippr.py',
-        'method.py'
-	],
+        os.path.join('sipprverse', 'sippr.py'),
+        os.path.join('sipprverse', 'method.py')
+    ],
     license='MIT',
     author='Adam Koziol',
     author_email='adam.koziol@inspection.gc.ca',
     description='Object oriented raw read typing software',
     url='https://github.com/OLC-Bioinformatics/sipprverse',
     long_description=open('README.md').read(),
-    install_requires=['olctools',
-                      'biopython',
-                      'pandas',
-                      'seaborn',
-                      'numpy',
-                      'pytest',
-                      'confindr',
-                      'validator_helper']
 )
