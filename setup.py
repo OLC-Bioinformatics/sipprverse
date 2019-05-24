48855cfb870693d64fb1f811f350aef88c07bb5e
adamkoziol <adam.koziol@inspection.gc.ca>
2019-05-24 08:36:37 -0400

    Added __init__.py to validation module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.28",
+    version="0.2.29",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
