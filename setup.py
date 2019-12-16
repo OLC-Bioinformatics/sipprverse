d1e1b0565169215ccef39028207b8e4c699dc35c
adamkoziol <adam.koziol@inspection.gc.ca>
2019-12-16 14:13:04 -0500

    Bump versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.44",
+    version="0.2.45",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
