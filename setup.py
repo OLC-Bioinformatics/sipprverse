101a4bca567e92941fc24a508880f52a0aa45f67
adamkoziol <adam.koziol@inspection.gc.ca>
2020-02-19 10:52:47 -0500

    Increment version for conda package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'adamkoziol'
 
 setup(
     name="sipprverse",
-    version="0.2.45",
+    version="0.2.46",
     packages=find_packages(),
     include_package_data=True,
     scripts=[
